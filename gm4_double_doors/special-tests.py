7a4658409d846bda2190c2d373f4af6442eca2af
SpecialBuilder <specialbuilder32@gmail.com>
2023-07-28 23:33:50 -0400

    Merge a sub-project copy for each wood type

--- gm4_double_doors/special-tests.py
+++ gm4_double_doors/special-tests.py
@@ -1,22 +1,29 @@
 from beet import subproject, Context
+from beet.contrib.vanilla import Vanilla
 
 def beet_default(ctx: Context):
-    ctx.require(subproject({
-        "data_pack":{
-            "load": [
-                {
-                    "data/gm4_double_doors/advancements/oak": "data/gm4_double_doors/templates/advancements",
-                    "data/gm4_double_doors/functions/oak": "data/gm4_double_doors/templates/functions",
-                    "data/gm4_double_doors/structures/oak": "data/gm4_double_doors/templates/structures",
+    vanilla = ctx.inject(Vanilla)
+    wooden_doors = vanilla.data.block_tags["minecraft:wooden_doors"]
+    
+    for wood_type in [s.removeprefix("minecraft:").removesuffix("_door") for s in wooden_doors.data["values"]]:
+        subproject_config = {
+            "data_pack":{
+                "load": [
+                    {
+                        f"data/gm4_double_doors/advancements/{wood_type}": "data/gm4_double_doors/templates/advancements",
+                        f"data/gm4_double_doors/functions/{wood_type}": "data/gm4_double_doors/templates/functions",
+                        f"data/gm4_double_doors/structures/{wood_type}": "data/gm4_double_doors/templates/structures",
+                    }
+                ],
+                "render":{
+                    "advancements": "*",
+                    "functions": "*",
+                    # "structures": "*"
                 }
-            ],
-            "render":{
-                "advancements": "*",
-                "functions": "*",
-                # "structures": "*"
+            },
+            "meta":{
+                "material_type": wood_type
             }
-        },
-        "meta":{
-            "material_name": "oak"
         }
-    }))
+
+        ctx.require(subproject(subproject_config))
