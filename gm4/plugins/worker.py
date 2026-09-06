8ce9b411a89e4b70a836b4c3bcf76ee52c0cc877
SpecialBuilder <specialbuilder32@gmail.com>
2026-09-06 13:22:10 -0400

    Revert "Attempt to write worker contexts to file"
    This reverts commit c25c92f38501a40ba9bb075d2c3fd05284fe03c7.

--- gm4/plugins/worker.py
+++ gm4/plugins/worker.py
@@ -2,7 +2,6 @@ from beet import Connection, Context, Pipeline, ResourcePack, DataPack
 from typing import Any
 import pickle
 from pathlib import Path
-import os
 
 RETRIEVE_ALL_PROJECTS = 0
 RETRIEVE_LAST_PROJECT = 1
@@ -86,14 +85,8 @@ def pickle_worker(ctx: Context):
     with ctx.worker(bridge) as channel:
         channel.send(INSPECT_ALL_PROJECTS)
     for project_storage_list in channel:
-        pickable_projects = []
-        for _, _, params in project_storage_list:
-            pickable_projects.append((DataPack(), ResourcePack(), params)) # don't send full DP or RP objects, just context entries
-
-        artifact_dir = Path("artifacts")
-        os.makedirs(artifact_dir, exist_ok=True)
-        with open(artifact_dir/"worker_contexts.pkl", "wb") as f:
-            pickle.dump(pickable_projects, f)
+        with open(Path("artifacts", "worker_contexts.pkl"), "wb") as f:
+            pickle.dump(project_storage, f)
 
 def unpickle_worker(ctx: Context):
     """Loads worker contexts from pickkle file"""
